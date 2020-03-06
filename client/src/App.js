042e062bf154bc4369bf49c84844d8e6bc5e30fe
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-06 10:59:17 +0100

    Add start scripts to main folder, change app to class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import MainLayout from './components/layouts/MainLayout/MainLayout';
 
-const App = () => {
-  return (
-    <MainLayout />
-  );
-}
+class App extends React.Component {
+
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/products')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ products: res });
+      });
+  }
+
+  render() {
+    return (
+      <MainLayout>
+      <div>
+        <ul>
+          {this.state.products.map(product => <li key={product.id}>{product.name}</li>)}
+        </ul>
+      </div>
+      </MainLayout>
+    );
+  }
+
+};
 
 export default App;
