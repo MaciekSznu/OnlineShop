1fc84e8f965384a2fa1786573d570d270a6e9fcf
Maciej Sznurawa <msznurawa@gmail.com>
2020-03-10 17:30:19 +0100

    Add SingleProduct: selector, aaction, thunk & reducer

--- client/src/redux/productsRedux.js
+++ client/src/redux/productsRedux.js
@@ -4,12 +4,12 @@ import { API_URL } from '../config';
 
 /* SELECTORS */
 export const getProducts = ({ products }) => products.data;
-export const getRequest = ({ products }) => products.request;
+export const getSingleProduct = ({ products}) => products.Singleproduct;
 
+export const getRequest = ({ products }) => products.request;
 export const startRequest = () => ({ type: START_REQUEST });
 export const endRequest = () => ({ type: END_REQUEST });
 export const errorRequest = (error) => ({ error, type: ERROR_REQUEST });
-
 
 // action name creator
 const reducerName = 'PRODUCTS';
@@ -17,6 +17,7 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 /* ACTIONS */
 export const LOAD_PRODUCTS = createActionName('LOAD_PRODUCTS');
+export const LOAD_SINGLE_PRODUCT = createActionName('LOAD_SINGLE_PRODUCT');
 
 export const START_REQUEST = createActionName('START_REQUEST');
 export const END_REQUEST = createActionName('END_REQUEST');
@@ -24,6 +25,7 @@ export const ERROR_REQUEST = createActionName('ERROR_REQUEST');
 
 
 export const loadProducts = payload => ({ payload, type: LOAD_PRODUCTS });
+export const loadSingleProduct = payload => ({ payload, type: LOAD_SINGLE_PRODUCT });
 
 /* THUNKS */
 export const loadProductsRequest = () => {
@@ -44,9 +46,27 @@ export const loadProductsRequest = () => {
   };
 };
 
+export const loadSingleProductRequest = () => {
+  return async dispatch => {
+
+    dispatch(startRequest());
+    try {
+
+      const res = await axios.get(`${API_URL}/products/${id}`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      dispatch(loadSingleProduct(res.data));
+      dispatch(endRequest());
+
+    } catch(e) {
+      dispatch(errorRequest(e.message));
+    }
+  }
+}
+
 /* INITIAL STATE */
 const initialState = {
   data: [],
+  singleProduct: null,
   request: {
     pending: false,
     error: null,
@@ -59,6 +79,8 @@ export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     case LOAD_PRODUCTS:
       return { ...statePart, data: action.payload };
+    case LOAD_SINGLE_PRODUCT:
+      return { ...statePart, singleProduct: action.payload };
     case START_REQUEST:
       return { ...statePart, request: { pending: true, error: null, success: null }};
     case END_REQUEST:
